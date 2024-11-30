ad168cc194d5e02dce8c8a536f7669f2ce5482cf
pedrammm068 <pedrampirdastan@gmail.com>
2024-11-30 22:53:20 +0330

    end no edit

--- app.js
+++ app.js
@@ -1,10 +1,66 @@
- const buot = document.getElementById("get-ht");
-const box = document.getElementById("ht-box");
 
-function clickHt(){
-    const inputVal = input.value
-    
-    const litodo = document.createElement("img")
-    litodo.src = inputVal
-    box.appendChild(litodo)
+
+
+
+const todo = document.getElementById("todos");
+const box = document.getElementById("ht-box");
+const list = document.getElementById("list")
+
+
+function clice() {
+    const val = todo.value
+  
+    if (val) {
+        const ing = `
+        <li id="${val}" style="red">
+        ${val}
+   
+         <button onclick="del(this)">delete</button>
+              <button onclick="ed(this)" >edit</button>
+               </li>
+        `
+
+        list.innerHTML += ing
+
+    }
 }
+
+
+
+function del(clickdel) {
+    clickdel.parentElement.remove()
+}
+function ed(editing){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function raded() {
+//     const ing = ad.map( rando => {
+//     return `
+//   <li id="${rando}" style="red">
+//         ${rando}
+   
+//          <button onclick="del(this)">delete</button>
+//               <button>edit</button>
+//                </li>   `
+// })
+// }
+// const temp = ing.join("/")
+
+// list.innerHTML = ing
